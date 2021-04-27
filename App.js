8674b42a2b429535a550b0e6e2589960606c0a87
Your Name <ardimanalu02@gmail.com>
2021-04-27 20:10:24 +0700

    new

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Instalasi Expo!</Text>
+      <Text>expo baru</Text>
       <StatusBar style="auto" />
     </View>
   );
